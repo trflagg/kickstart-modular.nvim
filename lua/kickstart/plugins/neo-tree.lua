24599e0851a14c3dca3f84734dc63fbd19c07fe4
Taylor Flagg <trflagg@gmail.com>
2025-04-12 12:47:09 -0500

    Modified neo-tree and telescope

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,6 +12,7 @@ return {
   cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>E', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
